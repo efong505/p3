6ad7b98035f532880c0cb8466266163ca2b22f9a
efong505 <efong505@nmsu.edu>
2022-12-22 13:41:00 -0700

    markdown

---
+++ learning_logs/templatetags/learning_logs_tag.py
@@ -0,0 +1,8 @@
+from django import template
+# from ..models import Entry
+register = template.Library()
+from django.utils.safestring import mark_safe
+import markdown
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
